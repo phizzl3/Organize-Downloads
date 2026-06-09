e246a2f9dd5c9fe0c1e0813340900834c7cb8d64
Bee <phizzl3@gmail.com>
2026-06-09 09:09:31 -0500

    moved file categories to separate file

---
+++ organize_downloads/file_categories.py
@@ -0,0 +1,9 @@
+# Map folder names to the file extensions they should hold
+FILE_CATEGORIES = {
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg", ".tiff", ".raw"],
+    "Documents": [".pdf", ".docx", ".doc", ".xlsx", ".xls", ".pptx", ".txt", ".csv", ".pages", ".numbers"],
+    "3D Printing & Design": [".stl", ".obj", ".3mf", ".step", ".stp", ".f3d", ".gcode"],
+    "Archives": [".zip", ".tar", ".gz", ".7z", ".rar", ".dmg"],
+    "Scripts & Code": [".py", ".sh", ".zsh", ".json", ".xml", ".html", ".css"],
+    "Audio & Video": [".mp3", ".wav", ".mp4", ".mov", ".mkv", ".avi"],
+}
--- organize_downloads/organize_downloads.py
+++ organize_downloads/organize_downloads.py
@@ -5,15 +5,15 @@ from pathlib import Path
 # Define the directory to organize (Tilde expands to your user profile)
 DOWNLOADS_DIR = Path.home() / "Downloads"
 
-# Map folder names to the file extensions they should hold
-FILE_CATEGORIES = {
-    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg", ".tiff", ".raw"],
-    "Documents": [".pdf", ".docx", ".doc", ".xlsx", ".xls", ".pptx", ".txt", ".csv", ".pages", ".numbers"],
-    "3D Printing & Design": [".stl", ".obj", ".3mf", ".step", ".stp", ".f3d", ".gcode"],
-    "Archives": [".zip", ".tar", ".gz", ".7z", ".rar", ".dmg"],
-    "Scripts & Code": [".py", ".sh", ".zsh", ".json", ".xml", ".html", ".css"],
-    "Audio & Video": [".mp3", ".wav", ".mp4", ".mov", ".mkv", ".avi"],
-}
+# # Map folder names to the file extensions they should hold
+# FILE_CATEGORIES = {
+#     "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".svg", ".tiff", ".raw"],
+#     "Documents": [".pdf", ".docx", ".doc", ".xlsx", ".xls", ".pptx", ".txt", ".csv", ".pages", ".numbers"],
+#     "3D Printing & Design": [".stl", ".obj", ".3mf", ".step", ".stp", ".f3d", ".gcode"],
+#     "Archives": [".zip", ".tar", ".gz", ".7z", ".rar", ".dmg"],
+#     "Scripts & Code": [".py", ".sh", ".zsh", ".json", ".xml", ".html", ".css"],
+#     "Audio & Video": [".mp3", ".wav", ".mp4", ".mov", ".mkv", ".avi"],
+# }
 
 # Extensions to safely ignore (active browser downloads)
 IGNORE_EXTENSIONS = [".crdownload", ".download", ".tmp"]
